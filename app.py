0a90d5ba3268bf497076009eca34ecaa5028c973
ImBarsikBR <guilhermedragon30@gmail.com>
2025-04-20 14:58:09 -0300

    Update Flask server configuration for Render deployment

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ def index():
 
     return render_template("index.html", tocando=False)
 
-if __name__ == '__main__':
-    app.run(debug=True) 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000))) 
